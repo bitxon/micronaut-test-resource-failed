65ad23a20c4077702531ff1d8bd98250f0e7eec3
Nikita Karpuk <bitxon.dev@gmail.com>
2022-08-25 18:55:24 +0200

    Add JPA entity

--- src/test/java/org/experiment/MicronautApplicationTest.java
+++ src/test/java/org/experiment/MicronautApplicationTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import jakarta.inject.Inject;
 
 @MicronautTest
-class MicronautTestResourceFailedTest {
+class MicronautApplicationTest {
 
     @Inject
     EmbeddedApplication<?> application;
